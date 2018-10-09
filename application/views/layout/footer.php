2525595960c67cc0ccf91bacfd07a207ec2522fe
Dominion Olorunfemi <dlorunfemi@gmail.com>
2018-10-09 15:06:21 +0100

    Version 1

---
+++ application/views/layout/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="pull-right">
+        ©<?php echo date('Y') ;?> Newschoolz | Developed by <a target="_blank" href="#">Newtinz</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
